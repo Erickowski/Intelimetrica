92911fcef151e21200d8da3ec2fdb858bef9cac2
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-10 16:23:31 -0500

    build

---
+++ build/precache-manifest.29350e185fb128c154088cea75be6225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cbc21efec8d401cbdf50a232864481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bcf304eb8eb77ca37ff",
+    "url": "/static/css/main.7dad5baf.chunk.css"
+  },
+  {
+    "revision": "30ed22306e56b2fa63c8",
+    "url": "/static/js/2.ad46fb5e.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.ad46fb5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcf304eb8eb77ca37ff",
+    "url": "/static/js/main.f1d1764c.chunk.js"
+  },
+  {
+    "revision": "abae383c3440cce13ef9",
+    "url": "/static/js/runtime-main.ceec6737.js"
+  }
+]);
